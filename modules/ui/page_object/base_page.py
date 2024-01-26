b55c3127e64d7f560bbb45ead97e6535d843b2b3
Tatyana Belyaeva <tat.bor.belyaeva@gmail.com>
2024-01-26 14:33:42 +0200

    Add GitHub UI tests

---
+++ modules/ui/page_object/base_page.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class BasePage:
+    DRIVER_NAME = "chromedriver"
+
+    def __init__(self) -> None:
+        self.driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()))
+
+    def close(self):
+        self.driver.close()
